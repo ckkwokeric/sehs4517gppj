f662ab9c5bf3804f3b16076dd71f514e97caec97
‘ckkwokeric’ <ckkwokeric112@gmail.com>
2022-04-19 17:32:29 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/nav', function () {
+    return view('layouts/nav');
+});
+Route::get('/footer', function () {
+    return view('layouts/footer');
+});
+Route::get('/home', function () {
+    return view('pages/home-community');
+});
+Route::get('/aboutus', function () {
+    return view('pages/aboutus');
+});
+Route::get('/activity', function () {
+    return view('pages/new/newactivities');
+});
+Route::get('/services', function () {
+    return view('pages/new/newservices');
+});
+Route::get('/services1', function () {
+    return view('pages/new/services1');
+});
+Route::get('/services2', function () {
+    return view('pages/new/services2');
+});
+Route::get('/services3', function () {
+    return view('pages/new/services3');
+});
+Route::get('/youth1', function () {
+    return view('pages/youths/youth1');
+});
+Route::get('/youth2', function () {
+    return view('pages/youths/youth2');
+});
+Route::get('/youth3', function () {
+    return view('pages/youths/youth3');
+});
+Route::get('/questionnaire', function () {
+    return view('pages/questionnaire/questionnaire');
+});
+Route::get('/score', function () {
+    return view('pages/questionnaire/score');
+});
+
+Route::get('/account', function () {
+    return view('pages/account/login');
+});
+Route::get('/login', function () {
+    return view('pages/account/login2');
+});
+Route::get('/registration', function () {
+    return view('pages/account/regist');
+});
