ac9b82d1eaae9223cae2ee220c677541b610f053
‘ckkwokeric’ <ckkwokeric112@gmail.com>
2022-04-21 00:28:49 +0800

    Fixed the formatting of all .blade.php files

--- resources/views/pages/account/login.blade.php
+++ resources/views/pages/account/login.blade.php
@@ -1,120 +1,136 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts --> 
+    <title>Laravel</title>
+
+    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/1ed92467bb.js" crossorigin="anonymous"></script> <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-       
-       
-        <style>
-        *{
-            margin:0;
-            padding:0;
+    <script src="https://kit.fontawesome.com/1ed92467bb.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
             box-sizing: border-box;
         }
-        
-        .body{
+
+        .body {
             background: #f5f6f7;
-            display:flex;
+            display: flex;
             justify-content: center;
             align-items: center;
             font-family: 'Montserrat', sans-serif;
-            height:100vh;
+            height: 100vh;
             margin: 0 auto;
         }
-        .container2{
-            background-color:white;
-            border-radius:10px;
-            width:768px;
-            max-width:100%;
-            min-height:480px;
-            position:relative;
-            overflow:hidden;
-            box-shadow:0px 14px 28px rgba(0,0, 0,0.25),
-                    0px 10px 10px rgba(0,0,0,0.22);
+
+        .container2 {
+            background-color: white;
+            border-radius: 10px;
+            width: 768px;
+            max-width: 100%;
+            min-height: 480px;
+            position: relative;
+            overflow: hidden;
+            box-shadow: 0px 14px 28px rgba(0, 0, 0, 0.25),
+                0px 10px 10px rgba(0, 0, 0, 0.22);
         }
-        .form-container{
+
+        .form-container {
             position: absolute;
-            top:0%;
-            height:100%;
+            top: 0%;
+            height: 100%;
             transition: all 0.6 ease-in-out;
         }
-        .sign-up-container{
-            left:0;
-            width:50%;
-            z-index:1;
+
+        .sign-up-container {
+            left: 0;
+            width: 50%;
+            z-index: 1;
             opacity: 0;
         }
-        form{
-            background-color:white;
-            display:flex;
+
+        form {
+            background-color: white;
+            display: flex;
             align-items: center;
             justify-content: center;
-            flex-direction:column;
-            padding:0px 50px;
-            height:100%;
-            text-align:center;
+            flex-direction: column;
+            padding: 0px 50px;
+            height: 100%;
+            text-align: center;
         }
-        form h1{
-            font-weight:bold;
-            margin:0;
+
+        form h1 {
+            font-weight: bold;
+            margin: 0;
         }
-        .social-container{
-            margin:20px 0;
+
+        .social-container {
+            margin: 20px 0;
         }
-        .social-container a{
-            border:1px solid gray;
+
+        .social-container a {
+            border: 1px solid gray;
             border-radius: 50%;
             display: inline-flex;
             justify-content: center;
             align-items: center;
             height: 40px;
             width: 40px;
-            margin:0 5px;
+            margin: 0 5px;
         }
-        .social-container a{
+
+        .social-container a {
             color: #333;
             font-size: 14px;
             text-decoration: none;
             margin: 15px 0;
         }
-        span{
+
+        span {
             font-size: 12px;
         }
-        input{
-            background-color:#eee;
+
+        input {
+            background-color: #eee;
             border: none;
             text-decoration: none;
             margin: 8px 0;
             padding: 12px 15px;
             width: 100%;
         }
-        .btn-sign, .press{
+
+        .btn-sign,
+        .press {
             border-radius: 20px;
             border: 1px solid #1E90Ff;
-            background-color:#1E90Ff;
-            color:white;
-            font-weight:bold;
+            background-color: #1E90Ff;
+            color: white;
+            font-weight: bold;
             padding: 12px 45px;
-            letter-spacing:1px;
+            letter-spacing: 1px;
             text-transform: uppercase;
-            transition:transform 0.8s ease-in ;
+            transition: transform 0.8s ease-in;
         }
-        .sign-in-container{
-            left:0;
-            width:50%;
-            z-index:2;
+
+        .sign-in-container {
+            left: 0;
+            width: 50%;
+            z-index: 2;
         }
-        .overlay-container{
+
+        .overlay-container {
             position: absolute;
-            top:0;
+            top: 0;
             left: 50%;
             width: 50%;
             height: 100%;
@@ -122,12 +138,13 @@
             z-index: 100;
             transition: transform 0.6s ease-in-out;
         }
-        .overlay{
-            
+
+        .overlay {
+
             background: linear-gradient(90deg, #0162c8, #55e7fc);
-            background-repeat:no-repeat ;
-            background-size:cover ;
-            background-position:0 0 ;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: 0 0;
             color: white;
             position: relative;
             left: -100%;
@@ -136,7 +153,8 @@
             transform: translateX(0);
             transition: transform 0.6s ease-in-out;
         }
-        .overlay-panel{
+
+        .overlay-panel {
             position: absolute;
             display: flex;
             align-items: center;
@@ -147,75 +165,93 @@
             top: 0;
             height: 100%;
             width: 50%;
-            transform:translateX(0) ;
+            transform: translateX(0);
             transition: transform 0.6s ease-in-out;
         }
-        .overlay-left{
+
+        .overlay-left {
             transform: translateX(-20%);
         }
-        .overlay-right{
-            right:0;
+
+        .overlay-right {
+            right: 0;
             transform: translateX(0);
         }
-        button:active{
-            transform:scale(0.95)
+
+        button:active {
+            transform: scale(0.95)
         }
-        button.press{
+
+        button.press {
             background-color: transparent;
-            border-color:white;
+            border-color: white;
         }
-        .overlay-panel p{
+
+        .overlay-panel p {
             font-size: 14px;
             font-weight: 100;
             line-height: 20px;
             letter-spacing: 0.5px;
             margin: 20px 0 30px;
         }
-        .container2.right-panel-active .sign-in-container{
+
+        .container2.right-panel-active .sign-in-container {
             transform: translateX(100%);
         }
-        .container2.right-panel-active .sign-up-container{
+
+        .container2.right-panel-active .sign-up-container {
             transform: translateX(100%);
             opacity: 1;
-            z-index:5;
+            z-index: 5;
             animation: slide 0.6s;
         }
-        @keyframes slide{
-            0%,49.99%{
+
+        @keyframes slide {
+
+            0%,
+            49.99% {
                 opacity: 0;
                 z-index: 1;
             }
-            50%,100%{
+
+            50%,
+            100% {
                 opacity: 1;
                 z-index: 5;
             }
         }
-        .container2.right-panel-active .overlay-container{
+
+        .container2.right-panel-active .overlay-container {
             transform: translateX(-100%);
         }
-        .container2.right-panel-active .overlay{
+
+        .container2.right-panel-active .overlay {
             transform: translateX(50%);
         }
-        .container2.right-panel-active .overlay-left{
+
+        .container2.right-panel-active .overlay-left {
             transform: translateX(0);
         }
-        .container2.right-panel-active .overlay-right{
+
+        .container2.right-panel-active .overlay-right {
             transform: translateX(20%);
         }
-                                 
-        </style>
-        
-@include('layouts.nav')
+    </style>
+</head>
+
 <!-- <body> -->
-<div class="body">
+
+<body>
+    @include('layouts.nav')
+    <div class="body">
         <div class="container2" id="container2">
             <div class="form-container sign-up-container">
                 <form action="">
                     <h1>Create Account</h1>
                     <div class="social-container">
-                        <a href=" "class="social"><i class="fa-brands fa-facebook-f"></i></a>
-                        <a href=" "class="social"><i class="fa-brands fa-google-plus-g"></i></a>
-                        <a href=" "class="social"><i class="fa-brands fa-linkedin-in"></i></a>
+                        <a href=" " class="social"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href=" " class="social"><i class="fa-brands fa-google-plus-g"></i></a>
+                        <a href=" " class="social"><i class="fa-brands fa-linkedin-in"></i></a>
                     </div>
                     <span>or use your gmail for registration</span>
                     <input type="text" placeholder="Username">
@@ -227,17 +263,17 @@
             </div>
             <div class="form-container sign-in-container">
                 <form action="">
-                        <h1>Sign In</h1>
-                        <div class="social-container">
-                            <a href=" "class="social"><i class="fa-brands fa-facebook-f"></i></a>
-                            <a href=" "class="social"><i class="fa-brands fa-google-plus-g"></i></a>
-                            <a href=" "class="social"><i class="fa-brands fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your Your account</span>
-                        <input type="text" placeholder="Username">
-                        <input type="pasword" placeholder="Password">
-                        <button class="btn-sign">Sign In</button>
-                    </form>
+                    <h1>Sign In</h1>
+                    <div class="social-container">
+                        <a href=" " class="social"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href=" " class="social"><i class="fa-brands fa-google-plus-g"></i></a>
+                        <a href=" " class="social"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                    <span>or use your Your account</span>
+                    <input type="text" placeholder="Username">
+                    <input type="pasword" placeholder="Password">
+                    <button class="btn-sign">Sign In</button>
+                </form>
             </div>
             <div class="overlay-container">
                 <div class="overlay">
@@ -255,6 +291,7 @@
             </div>
         </div>
     </div>
+
     <script>
         const sigeUpButton = document.getElementById("signUp");
         const sigeInButton = document.getElementById("signIn");
@@ -269,7 +306,6 @@
         })
     </script>
 
-
-
 </body>
+
 </html>
--- resources/views/pages/new/services1.blade.php
+++ resources/views/pages/new/services1.blade.php
@@ -1,66 +1,77 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-       
-       
-        <style>
-            .container2{
-                font-family: 'Montserrat', sans-serif;
-                margin-left:100px;
-                        margin-right:100px;
-                        justify-content: center;
-               
-                      
-            }
-            .container2 .poster{
-                width:100%;
-                margin-top:50px;
-                margin-bottom:70px;
-            }
-            .address{
-                display:flex;
-                font-weight: 400;
-                
-            }
-            .address .address-box{
-                display:flex;
-                padding-right:18px;
-            }
-            .address-box img{
-                padding-top:50%;
-            }
-            .address-box .type{
-                color:#D2B48C;
-            }
-            .arrow{
-                width:15px;
-                height:15px;
-                padding-top:50%;
-                padding-left:18px;
-            }
-            .content p{
-                padding-bottom:30px;
-                font-weight: 400;
-                font-size: 20px;
-                line-height: 24px;
-            }
-            .picture{
-                width:100%;
-              
-            }
-                  
-                                 
-        </style>
-        
-@include('layouts.nav')
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+    <style>
+        .container2 {
+            font-family: 'Montserrat', sans-serif;
+            margin-left: 100px;
+            margin-right: 100px;
+            justify-content: center;
+
+
+        }
+
+        .container2 .poster {
+            width: 100%;
+            margin-top: 50px;
+            margin-bottom: 70px;
+        }
+
+        .address {
+            display: flex;
+            font-weight: 400;
+
+        }
+
+        .address .address-box {
+            display: flex;
+            padding-right: 18px;
+        }
+
+        .address-box img {
+            padding-top: 50%;
+        }
+
+        .address-box .type {
+            color: #D2B48C;
+        }
+
+        .arrow {
+            width: 15px;
+            height: 15px;
+            padding-top: 50%;
+            padding-left: 18px;
+        }
+
+        .content p {
+            padding-bottom: 30px;
+            font-weight: 400;
+            font-size: 20px;
+            line-height: 24px;
+        }
+
+        .picture {
+            width: 100%;
+
+        }
+    </style>
+</head>
+
 <!-- <body> -->
+
+<body>
+    @include('layouts.nav')
+
     <div class="container2">
         <img class="poster" src="img/new-program.png" alt="">
         <div class="address">
@@ -77,29 +88,35 @@
                 <span><img class="arrow" src="img/arrow.png" alt=""></span>
             </div>
         </div>
-        
+
         <div class="content">
-            <p>Christian Wisdom Service has always been concerned about the physical, psychological, 
-                social and spiritual needs of the carers. Therefore, the "Wisdom and Love Carer Support Project" was established 
+            <p>Christian Wisdom Service has always been concerned about the physical, psychological,
+                social and spiritual needs of the carers. Therefore, the "Wisdom and Love Carer Support Project" was
+                established
                 with the concept of "carer-oriented" service to walk with the carers of the mentally disabled.
             </p>
-            <p>Christian Wisdom Service has always been concerned about the physical, psychological, social and spiritual needs 
-                of the carers. Therefore, the "Wisdom and Love Carer Support Project" was established with the concept of "carer-oriented"
-                 service to walk with the carers of the mentally disabled.
+            <p>Christian Wisdom Service has always been concerned about the physical, psychological, social and
+                spiritual needs
+                of the carers. Therefore, the "Wisdom and Love Carer Support Project" was established with the concept
+                of "carer-oriented"
+                service to walk with the carers of the mentally disabled.
             </p>
-            <p>In addition, in order to value the carers' participation in the center and to enhance their sense of recognition 
-                and belonging to the service, we have established a membership system and a "Wisdom and Love Committee", with the 
+            <p>In addition, in order to value the carers' participation in the center and to enhance their sense of
+                recognition
+                and belonging to the service, we have established a membership system and a "Wisdom and Love Committee",
+                with the
                 carers acting as office-bearers, with the direction of joint planning and sharing of tasks.
             </p>
-            <p>If you are interested in participating or collaborating in the following events or service briefings, 
+            <p>If you are interested in participating or collaborating in the following events or service briefings,
                 please click here to leave your contact information so that our staff can contact you.
             </p>
         </div>
     </div>
-    <img  class="picture" src="img/Component 57.png" alt="">
+    <img class="picture" src="img/Component 57.png" alt="">
 
 
-@include('layouts.footer')
+    @include('layouts.footer')
 
 </body>
+
 </html>
